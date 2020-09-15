5b7b1c1f7bdf766d04d4e9d0439dd737a4252db6
govardhanreddy yellu <gvrr519@gmail.com>
2020-09-15 09:52:25 +0530

    java file hello

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello{
-public static void main(string[] args)
+public static void main(String[] args)
 {
 	//print hellow orld in the terminal window
 	System.out.println("hello govardhan world");
